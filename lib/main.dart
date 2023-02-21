fd097b208e53f8e9a47286df53976d44bfce896c
Florian <florian.bailly42@gmail.com>
2023-02-21 20:57:53 +0100

    ajout layout page d'accueil

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,23 @@ import 'package:vitaville/signalements_page.dart';
 import 'package:vitaville/idees_page.dart';
 import 'package:vitaville/sondages_page.dart';
 import 'package:vitaville/profile_page.dart';
-
+import 'package:vitaville/accueil_page.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key : key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey
-      ),
+      theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: const RootPage(),
     );
   }
-  
 }
 
 class RootPage extends StatefulWidget {
@@ -48,18 +45,20 @@ class _RootPageState extends State<RootPage> {
       body: pages[currentPage],
       bottomNavigationBar: NavigationBar(
         destinations: const [
-          NavigationDestination(icon: Icon(Icons.newspaper), label: 'Actualités'),
+          NavigationDestination(
+              icon: Icon(Icons.newspaper), label: 'Actualités'),
           NavigationDestination(icon: Icon(Icons.how_to_vote), label: 'Idées'),
-          NavigationDestination(icon: Icon(Icons.warning_amber_rounded), label: 'Signaler'),
+          NavigationDestination(
+              icon: Icon(Icons.warning_amber_rounded), label: 'Signaler'),
           NavigationDestination(icon: Icon(Icons.poll), label: 'Sondages'),
           NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
         ],
-        onDestinationSelected: (int index){
+        onDestinationSelected: (int index) {
           setState(() {
             currentPage = index;
           });
-          },
-          selectedIndex: currentPage,
+        },
+        selectedIndex: currentPage,
       ),
     );
   }
