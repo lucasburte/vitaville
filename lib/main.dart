95ad5e82c431b8dd029e47cb8ae2419ceb82ca11
LeoHum <leohumbert@laposte.net>
2023-03-25 16:45:07 +0100

    résolution bug navbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:vitaville/accueil_page.dart';
-import 'package:vitaville/actu_page.dart';
-import 'package:vitaville/signalements_page.dart';
-import 'package:vitaville/idees_page.dart';
-import 'package:vitaville/sondages_page.dart';
-import 'package:vitaville/Profile/profile_page.dart';
 import 'package:vitaville/states/current_user.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -29,45 +24,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(primarySwatch: Colors.blueGrey),
         home: const AccueilPage(),//changer par RootPage() parce qu'en l'état la connexion fonctionne mais qd on arrive sur les actus il y a pas de navbar
-      ),
-    );
-  }
-}
-
-class RootPage extends StatefulWidget {
-  const RootPage({super.key});
-
-  @override
-  State<RootPage> createState() => _RootPageState();
-}
-
-class _RootPageState extends State<RootPage> {
-  int currentPage = 0;
-  List<Widget> pages = const [
-    ActuPage(),
-    IdeesPage(),
-    SignalementsPage(),
-    SondagesPage(),
-    ProfilePage()
-  ];
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[currentPage],
-      bottomNavigationBar: NavigationBar(
-        destinations: const [
-          NavigationDestination(icon: Icon(Icons.newspaper), label: 'Actualités'),
-          NavigationDestination(icon: Icon(Icons.how_to_vote), label: 'Idées'),
-          NavigationDestination(icon: Icon(Icons.warning_amber_rounded), label: 'Signaler'),
-          NavigationDestination(icon: Icon(Icons.poll), label: 'Sondages'),
-          NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
-        ],
-        onDestinationSelected: (int index) {
-          setState(() {
-            currentPage = index;
-          });
-        },
-        selectedIndex: currentPage,
       ),
     );
   }
---
+++ lib/nav_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:vitaville/actu_page.dart';
+import 'package:vitaville/signalements_page.dart';
+import 'package:vitaville/idees_page.dart';
+import 'package:vitaville/sondages_page.dart';
+import 'package:vitaville/Profile/profile_page.dart';
+
+
+
+class NavPage extends StatefulWidget {
+  const NavPage({super.key});
+
+  @override
+  State<NavPage> createState() => _NavPageState();
+}
+
+class _NavPageState extends State<NavPage> {
+  int currentPage = 0;
+  List<Widget> pages = const [
+    ActuPage(),
+    IdeesPage(),
+    SignalementsPage(),
+    SondagesPage(),
+    ProfilePage(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentPage], // en fait ça ça fait que le body de RootPage c'est la page de la liste pages qui s'affiche
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.newspaper), label: 'Actualités'),
+          NavigationDestination(icon: Icon(Icons.how_to_vote), label: 'Idées'),
+          NavigationDestination(icon: Icon(Icons.warning_amber_rounded), label: 'Signaler'),
+          NavigationDestination(icon: Icon(Icons.poll), label: 'Sondages'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profil'),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
+  }
+}
