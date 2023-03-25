62688d51b31740644f5504fd7ddb448d50fd7f82
c.est.manuella.c <c.est.manuella.c@gmail.com>
2023-03-25 21:42:00 +0100

    Ajout d'une map dans boite à idées + ajout des dépendances correspondantes dans fichiers pubspec

--- lib/idees_page.dart
+++ lib/idees_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class IdeesPage extends StatelessWidget {
   const IdeesPage({super.key});
@@ -20,27 +22,66 @@ class IdeasHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Boite à idées",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-          ),
+      body: FlutterMap(
+        mapController: null,
+        options: MapOptions(
+          center: LatLng(48.69821313814409, 6.18658746494504),
+          zoom: 15,
         ),
-        backgroundColor: const Color(0xFFFFFBFE), //Couleur utilisée sur Figma
-        elevation: 0, //Retire l'ombre sous l'Appbar
-        centerTitle: true, //Permet de centrer le texte
+        nonRotatedChildren: [
+          AttributionWidget.defaultWidget(
+            source: '© OpenStreetMap contributors',
+            onSourceTapped: () {},
+          ),
+        ],
+        children: [
+              TileLayer(
+                urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+                userAgentPackageName: 'com.vitaville.app',
+              ),
+              MarkerLayer(
+                markers: [
+                  Marker(
+                    point: LatLng(48.69821313814409, 6.18658746494504),
+                    width: 40,
+                    height: 40,
+                    builder: (context) => Icon(
+                      Icons.location_on,
+                      color: Colors.black,
+                      size: 40.0,
+                      semanticLabel: 'name of place',
+                    ),
+                  ),
+                ],
+              ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const IdeaForm()),
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
+      floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const IdeaForm()),
+                );
+              },
+              child: const Icon(Icons.my_location),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const IdeaForm()),
+                );
+              },
+              child: const Icon(Icons.add),
+            ),
+          ]
+      )
     );
   }
 }
