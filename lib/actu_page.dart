1a7ba5572fcbf1ede5299a3eff73fe44c578ed4c
LeoHum <leohumbert@laposte.net>
2023-03-27 18:10:04 +0200

    amélioration page actu + détail actu

--- lib/actu_page.dart
+++ lib/actu_page.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'actus.dart';
-import 'package:intl/intl.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+import 'package:vitaville/detail_actu.dart';
 
 class ActuPage extends StatefulWidget {
   const ActuPage({super.key});
@@ -29,77 +27,7 @@ class _ActuPageState extends State<ActuPage> {
       ),
     );
   }
-  Widget buildFloatingSearchBar() {
-  final isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
 
-  return FloatingSearchBar(
-    hint: 'Rechercher un post',
-    scrollPadding: const EdgeInsets.only(top: 16, bottom: 10),
-    transitionDuration: const Duration(milliseconds: 800),
-    transitionCurve: Curves.easeInOut,
-    physics: const BouncingScrollPhysics(),
-    axisAlignment: isPortrait ? 0.0 : -1.0,
-    openAxisAlignment: 0.0,
-    width: isPortrait ? 300 : 250,
-    debounceDelay: const Duration(milliseconds: 500),
-    onQueryChanged: (query) {
-      // Call your model, bloc, controller here.
-    },
-    // Specify a custom transition to be used for
-    // animating between opened and closed stated.
-    transition: CircularFloatingSearchBarTransition(),
-    actions: [
-      FloatingSearchBarAction(
-        showIfOpened: false,
-        child: CircularButton(
-          icon: const Icon(Icons.place),
-          onPressed: () {},
-        ),
-      ),
-      FloatingSearchBarAction.searchToClear(
-        showIfClosed: false,
-      ),
-    ],
-    builder: (context, transition) {
-      return ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Material(
-          color: Colors.white,
-          elevation: 4.0,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: Colors.accents.map((color) {
-              return Container(height: 112, color: color);
-            }).toList(),
-          ),
-        ),
-      );
-    },
-  );
-}
-
-  @override
-  State<ActuPage> createState() => _ActuPageState();
-}
-
-class _ActuPageState extends State<ActuPage> {
-  List<String> docIDs = []; //liste des id des actus
-  final TextEditingController _searchController = TextEditingController(); //controller pour la barre de recherche
-
-
-  Future getDocId() async { //à chaque reload, vide la liste des actus pour ne pas avoir de doublons
-    if(docIDs.isNotEmpty){
-      docIDs.clear();
-    }
-    await FirebaseFirestore.instance.collection('actus').get().then(
-          (snapshot) => snapshot.docs.forEach(
-            (document) {
-          print(document.reference);
-          docIDs.add(document.reference.id); //add tous les docIDs pour chaque refresh --> doublons dans la liste des actus affichées
-        },
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -121,13 +49,12 @@ class _ActuPageState extends State<ActuPage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              //buildFloatingSearchBar(), //search bar qui marche pas
+              CustomSearchContainer(), //search bar qui marche pas
               Expanded(
                 child: FutureBuilder(
                     future: getDocId(),
                     builder: (context, snapshot) {
                       return ListView.builder(
-                        
                         itemCount: docIDs.length,
                         itemBuilder: (BuildContext context, int index) {
                           return Padding(
@@ -136,10 +63,6 @@ class _ActuPageState extends State<ActuPage> {
                               title: GetActus(
                                 documentId: docIDs[index],
                               ),
-                              onTap: () {
-                                return null;
-                                //navigator.push(material route page détail actu)
-                              },
                             ),
                           );
                         },
@@ -148,7 +71,8 @@ class _ActuPageState extends State<ActuPage> {
               )
             ],
           ),
-        ));
+        )
+      );
   }
 }
 
@@ -169,121 +93,176 @@ class GetActus extends StatelessWidget {
       future: actus.doc(documentId).get(),
       builder: (((context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done) {
+          final DocumentSnapshot documentSnapshot = snapshot.data!;
           Map<String, dynamic> data =
               snapshot.data!.data() as Map<String, dynamic>;
-          return Container(
-            key: _key1,
-            decoration: BoxDecoration(
-                color: const Color(0xFFFFFBFE),
-                border: Border.all(color: Colors.grey),
-                borderRadius: BorderRadius.circular(10),
-                boxShadow: const [
-                  BoxShadow(
-                    color: Colors.grey,
-                    blurRadius: 10.0,
-                    spreadRadius: 0.5,
-                    offset: Offset(
-                      4.0,
-                      4.0,
+          return InkWell(
+            onTap: (() {
+              Navigator.push(context, MaterialPageRoute(builder: ((context) => DetailActuPage(documentSnapshot: documentSnapshot))));
+            }),
+            child: Container(
+              key: _key1,
+              decoration: BoxDecoration(
+                  color: const Color(0xFFFFFBFE),
+                  border: Border.all(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(10),
+                  boxShadow: const [
+                    BoxShadow(
+                      color: Colors.grey,
+                      blurRadius: 10.0,
+                      spreadRadius: 0.1,
+                      offset: Offset(
+                        1.0,
+                        1.0,
+                      ),
+                    )
+                  ]),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: Row(//titre, date publication, icones fav, calendrier, share
+                      children: [
+                        Column(
+                          children: [
+                            SizedBox(
+                              height: 68,
+                              width: 245,
+                              child: ListTile(
+                                  title :Text('${data['title']}',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 17.0,
+                                    ),
+                                  ),
+                                  //Text(DateTime.fromMillisecondsSinceEpoch(data['publication_date']*1000)), //TimeStamp to DateTime to String
+                                  subtitle: const Text('il y a un jour'), //en attendant d'arriver à mettre la date de publication
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Icon(Icons.star_border, color: Colors.black,),
+                                SizedBox(width: 5,),
+                                Icon(Icons.calendar_month_outlined, color: Colors.black,),
+                                SizedBox(width: 5,),
+                                Icon(Icons.share, color: Colors.black,),
+                              ],
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                  Row(
+                    //photo d'illustration
+                    children: [
+                      Image.asset("lib/assets/images/eau.jpg",
+                      width: 342,//récupérer la largeur du container
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 10,),
+                  Row(
+                    //date, lieu
+                    children: [
+                      Text('   '+'${data['place']}' + ' le 14/04/2023',
+                      style: const TextStyle(
+                        color: Colors.grey,
+                      ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    //description
+                    children: [
+                      Flexible(child: Padding(padding: const EdgeInsets.all(10), child: Text(data['description']))), //idéal : n'afficher qu'une partie du long texte
+                    ],
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        Card(
+                          color: const Color.fromARGB(255, 139, 71, 113),
+                          child: SizedBox(
+                            width: 70,
+                            height: 25,
+                            child: Center(child: Text('${data['tags'][0]}',style: TextStyle(fontStyle: FontStyle.italic, color: Colors.white,),)),
+                          ),
+                        ),
+                        Card(
+                          color: const Color.fromARGB(255, 139, 71, 113),
+                          child: SizedBox(
+                            width: 70,
+                            height: 25,
+                            child: Center(child: Text('${data['tags'][1]}',style: TextStyle(fontStyle: FontStyle.italic, color: Colors.white),)),
+                          ),
+                        ),//voir comment afficher le nombre de tags correct
+                      ],
                     ),
                   )
-                ]),
-            child: Column(
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(5.0),
-                  child: Row(
-                    //titre, date publication, icones fav, calendrier, share
-                    children: [
-                      Column(
-                        children: [
-                          SizedBox(
-                            height: 68,
-                            width: 245,
-                            child: ListTile(
-                                title :Text('${data['title']}',
-                                  style: const TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 17.0,
-                                  ),
-                                ),
-                                //Text(DateTime.fromMillisecondsSinceEpoch(data['publication_date']*1000)), //TimeStamp to DateTime to String
-                                subtitle: const Text('il y a un jour'), //en attendant d'arriver à mettre la date de publication
-                            ),
-                          ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Icon(Icons.star_border, color: Colors.black,),
-                              SizedBox(width: 5,),
-                              Icon(Icons.calendar_month_outlined, color: Colors.black,),
-                              SizedBox(width: 5,),
-                              Icon(Icons.share, color: Colors.black,),
-                            ],
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                Row(
-                  //photo d'illustration
-                  children: [
-                    Image.asset("lib/assets/images/eau.jpg",
-                    width: 342,//récupérer la largeur du container
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10,),
-                Row(
-                  //date, lieu
-                  children: [
-                    Text('   '+'${data['place']}' + ' le 14/04/2023',
-                    style: const TextStyle(
-                      color: Colors.grey,
-                    ),
-                    ),
-                  ],
-                ),
-                Row(
-                  //description
-                  children: [
-                    Flexible(child: Padding(padding: const EdgeInsets.all(10), child: Text(data['description']))), //regarder comment n'afficher qu'une partie du long texte
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Card(
-                        color: Theme.of(context).colorScheme.surfaceVariant,
-                        child: SizedBox(
-                          width: 70,
-                          height: 25,
-                          child: Center(child: Text('${data['tags'][0]}',style: TextStyle(fontStyle: FontStyle.italic),)),
-                        ),
-                      ),
-                      Card(
-                        color: Theme.of(context).colorScheme.surfaceVariant,
-                        child: SizedBox(
-                          width: 70,
-                          height: 25,
-                          child: Center(child: Text('${data['tags'][1]}',style: TextStyle(fontStyle: FontStyle.italic),)),
-                        ),
-                      ),//Text('${data['tags'][2]}'), //voir comment afficher le nombre de tags correct
-                    ],
-                  ),
-                )
-              ],
+                ],
+              ),
             ),
           );
         }
-        return Text("Chargement...");
+        return const Text("Chargement...");
       })),
     );
   }
 }
 
+class CustomSearchContainer extends StatelessWidget { // Widget barre de recherche
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16, 10, 16, 8),
+      child: Container(
+        height: 50,
+
+        decoration: BoxDecoration(color: Color.fromARGB(255, 235, 235, 235), borderRadius: BorderRadius.circular(30),),
+        child: Row(
+          children: <Widget>[
+            SizedBox(width: 16),
+            CustomUserAvatar(),
+            CustomTextField(),
+            SizedBox(width: 16),
+            Icon(Icons.search),
+            SizedBox(width: 16),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: TextFormField(
+        maxLines: 1,
+        decoration: InputDecoration(
+          contentPadding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
+          hintText: "Rechercher un post",
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
+
+class CustomUserAvatar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 32,
+      width: 32,
+      decoration: BoxDecoration(color: Colors.grey[500], borderRadius: BorderRadius.circular(16)),
+    );
+  }
+}
